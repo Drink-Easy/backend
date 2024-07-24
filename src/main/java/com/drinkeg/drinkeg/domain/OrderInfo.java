9ac568aedc3ab780518491906c4920edc7e44e5f
Jeongyeon Park <githubb0208@gmail.com>
2024-07-25 00:24:19 +0900

    Fix : rename Order entity to OrderInfo

--- src/main/java/com/drinkeg/drinkeg/domain/OrderInfo.java
+++ src/main/java/com/drinkeg/drinkeg/domain/OrderInfo.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Order {
+public class OrderInfo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/drinkeg/drinkeg/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.drinkeg.drinkeg.repository;
-
-import com.drinkeg.drinkeg.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
