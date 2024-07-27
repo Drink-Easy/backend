1903057a671b65a11002bbc7173ec132ef85e83a
jeongyun <s6570490@naver.com>
2024-07-27 20:28:50 +0900

    Rename: 이름 수정 및 서비스 클래스 추가 - Request 엔티티를 Inquiry 엔티티로 수정 - RequestRepository를 InquriyRepository로 수정 - FeedbackService, InquiryService 생성

---
+++ src/main/java/com/drinkeg/drinkeg/repository/InquiryRepository.java
@@ -0,0 +1,6 @@
+package com.drinkeg.drinkeg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
+}
--- src/main/java/com/drinkeg/drinkeg/repository/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.drinkeg.drinkeg.repository;
-
-import com.drinkeg.drinkeg.domain.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
