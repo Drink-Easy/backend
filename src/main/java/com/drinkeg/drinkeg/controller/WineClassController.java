6f763c0224127ed6c3074754bad0b66c47a43f49
노정윤 <noh-jeongyun@nojeong-yun-ui-noteubug.local>
2024-07-31 15:20:37 +0900

    #3 Refactor: api 엔드포인트 수정 - wine class 관련 엔드포인트 wineClass에서 wine-class로 수정.

--- src/main/java/com/drinkeg/drinkeg/controller/WineClassController.java
+++ src/main/java/com/drinkeg/drinkeg/controller/WineClassController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/wineclass")
+@RequestMapping("/wine-class")
 public class WineClassController {
 
     private final WineClassService wineClassService;
