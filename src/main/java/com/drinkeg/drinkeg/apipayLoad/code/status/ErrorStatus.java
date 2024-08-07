6f098d30cb7b3b1da3993033e21a0ace0e31c677
노정윤 <noh-jeongyun@nojeong-yun-ui-noteubug.local>
2024-08-07 16:44:36 +0900

    #7 Feat: 와인 뉴스 북마크 기능 구현 - 와인 뉴스 북마크 생성 구현 - 유저의 와인 뉴스 북마크 조회 구현 - 와인 클래스 뉴스 삭제 구현

--- src/main/java/com/drinkeg/drinkeg/apipayLoad/code/status/ErrorStatus.java
+++ src/main/java/com/drinkeg/drinkeg/apipayLoad/code/status/ErrorStatus.java
@@ -13,6 +13,10 @@ public enum ErrorStatus implements BaseCode {
 
     WINECLASS_NOT_FOUND(HttpStatus.BAD_REQUEST, "WINECLASS4001", "와인 클래스가 없습니다."),
 
+    // WineNewsBookMark Error
+    WINE_NEWS_BOOKMARK_NOT_FOUND(HttpStatus.BAD_REQUEST, "WINENWSBOOKMARK4001", "와인 뉴스 북마크가 없습니다."),
+    WINE_NEWS_BOOKMARK_DUPLICATED(HttpStatus.BAD_REQUEST, "WINENWSBOOKMARK4002", "이미 존재하는 북마크입니다."),
+
     // Member Error
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "MEMBER4001", "회원이 없습니다."),
 
