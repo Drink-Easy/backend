7be233d3a6f647bb41ba11169c6352ed934a7506
노정윤 <noh-jeongyun@nojeong-yun-ui-noteubug.local>
2024-08-12 19:20:11 +0900

    #6 Refactor: validation 설정

--- src/main/java/com/drinkeg/drinkeg/dto/WineClassBookMarkDTO/request/WineClassBookMarkRequestDTO.java
+++ src/main/java/com/drinkeg/drinkeg/dto/WineClassBookMarkDTO/request/WineClassBookMarkRequestDTO.java
@@ -1,6 +1,7 @@
 package com.drinkeg.drinkeg.dto.WineClassBookMarkDTO.request;
 
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class WineClassBookMarkRequestDTO {
+    @NotNull(message="와인 클래스 ID는 필수입니다.")
     private Long wineClassId;
 }
