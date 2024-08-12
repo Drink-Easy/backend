cf5b8861814c9ef0e82ba7c33ba55404fbcc7839
dayeong <dayeongyun184@gmail.com>
2024-08-12 21:50:06 +0900

    #20 feature/ s3 delete 구현

--- src/main/java/com/drinkeg/drinkeg/S3/AmazonS3Manager.java
+++ src/main/java/com/drinkeg/drinkeg/S3/AmazonS3Manager.java
@@ -1,6 +1,9 @@
 package com.drinkeg.drinkeg.S3;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.drinkeg.drinkeg.config.S3Config;
@@ -22,7 +25,6 @@ public class AmazonS3Manager{
 
     private final S3Config s3Config;
 
-    private final UuidRepository uuidRepository;
 
     public String uploadFile(String keyName, MultipartFile file){
 
@@ -37,9 +39,22 @@ public class AmazonS3Manager{
         return amazonS3.getUrl(s3Config.getBucket(), keyName).toString();
     }
 
+    // S3에 들어갈 KeyName 만들어주기 (어떤 디렉토리 안으로 들어갈지 반영)
     public String generateWineNewsKeyName(Uuid uuid) {
         return s3Config.getWineNewsPaths() + '/' + uuid.getUuid();
     }
 
+
+    public void deleteFile(String fileName){
+        try {
+            amazonS3.deleteObject(new DeleteObjectRequest(s3Config.getBucket(), fileName));
+        }catch (AmazonServiceException e) {
+            // AWS 서비스에서 발생한 오류 처리
+            log.error("Amazon S3 service error: {}", e.getErrorMessage(), e);
+        } catch (SdkClientException e) {
+            // 클라이언트 측에서 발생한 오류 처리
+            log.error("Amazon S3 client error: {}", e.getMessage(), e);
+        }
+    }
 
 }
--- src/main/java/com/drinkeg/drinkeg/S3/S3TestController.java
+++ src/main/java/com/drinkeg/drinkeg/S3/S3TestController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -20,13 +17,19 @@ import org.springframework.web.multipart.MultipartFile;
 public class S3TestController {
 
     private final S3Service s3Service;
-
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
     public ApiResponse<String> upload(@AuthenticationPrincipal PrincipalDetail principalDetail, @RequestPart(value = "request") S3TestUploadDTO testUploadDTO,
                                       @RequestPart(value = "picture", required = false) MultipartFile picture) {
 
         String s = s3Service.SaveImage(picture);
         return ApiResponse.onSuccess("사진 저장 완료");
+    }
+
+    @DeleteMapping("/delete")
+    public ApiResponse<String> delete(@AuthenticationPrincipal PrincipalDetail principalDetail, @RequestParam("picture") String picture) {
+
+        s3Service.deleteFile(picture);
+        return ApiResponse.onSuccess("사진 삭제 완료");
     }
 
 }
