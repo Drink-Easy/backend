c51e25152a243e6f9c3d4d0571336f7f4bb9889e
jeongyeon0208 <githubb0208@gmail.com>
2024-08-16 01:18:34 +0900

    #1 Refactor : 필요없는 Import 제거 (TastingNoteController)

--- src/main/java/com/drinkeg/drinkeg/controller/TastingNoteController.java
+++ src/main/java/com/drinkeg/drinkeg/controller/TastingNoteController.java
@@ -9,7 +9,6 @@ import com.drinkeg.drinkeg.dto.TastingNoteDTO.response.NoteResponseDTO;
 import com.drinkeg.drinkeg.dto.loginDTO.commonDTO.PrincipalDetail;
 import com.drinkeg.drinkeg.service.memberService.MemberService;
 import com.drinkeg.drinkeg.service.tastingNoteService.TastingNoteService;
-import com.drinkeg.drinkeg.service.wineService.WineService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,7 +22,6 @@ import java.util.List;
 public class TastingNoteController {
 
     private final TastingNoteService tastingNoteService;
-    private final WineService wineService;
     private final MemberService memberService;
 
     // 새 노트 작성
