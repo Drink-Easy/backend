17cc9825b4635b454c96e4cd2ed312dbb5098175
노정윤 <noh-jeongyun@nojeong-yun-ui-noteubug.local>
2024-08-19 22:33:54 +0900

    #76 Feat: WineSale 기능 구현 - 추후에 인증/인가 작업 필요

--- src/main/java/com/drinkeg/drinkeg/repository/WineSaleRepository.java
+++ src/main/java/com/drinkeg/drinkeg/repository/WineSaleRepository.java
@@ -1,7 +1,16 @@
 package com.drinkeg.drinkeg.repository;
 
+import com.drinkeg.drinkeg.domain.Wine;
 import com.drinkeg.drinkeg.domain.WineSale;
+import com.drinkeg.drinkeg.domain.WineStore;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface WineSaleRepository extends JpaRepository<WineSale, Long> {
+    List<WineSale> findAllByWineStore(WineStore wineStore);
+
+    List<WineSale> findAllByWine(Wine wine);
+
 }
