3992e3131c1b898daeafc7720c42b5ce8226e881
노정윤 <noh-jeongyun@nojeong-yun-ui-noteubug.local>
2024-09-20 21:11:31 +0900

    #114 Feat: 와인 강의 수강 여부 Controller 작성

--- src/main/java/com/drinkeg/drinkeg/controller/WineLectureCompleteController.java
+++ src/main/java/com/drinkeg/drinkeg/controller/WineLectureCompleteController.java
@@ -1,11 +1,36 @@
 package com.drinkeg.drinkeg.controller;
 
+import com.drinkeg.drinkeg.apipayLoad.ApiResponse;
+import com.drinkeg.drinkeg.dto.WineLectureCompleteDTO.response.WineLectureCompleteResponseDTO;
+import com.drinkeg.drinkeg.dto.loginDTO.commonDTO.PrincipalDetail;
+import com.drinkeg.drinkeg.service.WineLectureCompleteService.WineLectureCompleteService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/wine-lecture-complete")
 public class WineLectureCompleteController {
+    private final WineLectureCompleteService wineLectureCompleteService;
+
+    @GetMapping("/")
+    public ApiResponse<List<WineLectureCompleteResponseDTO>> getWineLectureCompleteByMember(@AuthenticationPrincipal PrincipalDetail principalDetail) {
+        List<WineLectureCompleteResponseDTO> wineLectureCompleteResponseDTOS = wineLectureCompleteService.showWineLectureCompleteByMember(principalDetail);
+        return ApiResponse.onSuccess(wineLectureCompleteResponseDTOS);
+    }
+
+    @PostMapping("/wine-class/{wineClassId}")
+    public ApiResponse<WineLectureCompleteResponseDTO> createWineLectureComplete(@PathVariable Long wineClassId, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        WineLectureCompleteResponseDTO wineLectureCompleteResponseDTO = wineLectureCompleteService.saveWineLectureComplete(wineClassId, principalDetail);
+        return ApiResponse.onSuccess(wineLectureCompleteResponseDTO);
+    }
+
+    @DeleteMapping("/{wineLectureCompleteId}")
+    public ApiResponse<String> deleteWineLectureComplete(@PathVariable Long wineLectureCompleteId, @AuthenticationPrincipal PrincipalDetail principalDetail) {
+        wineLectureCompleteService.deleteWineLectureCompleteById(wineLectureCompleteId, principalDetail);
+        return ApiResponse.onSuccess("와인 강의 수강여부 삭제 완료");
+    }
 }
