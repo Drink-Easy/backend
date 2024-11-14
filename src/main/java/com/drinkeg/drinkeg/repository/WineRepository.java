ec50c4200cb6e5233d9bc0f1895dfe077a1a00df
jeongyeon0208 <githubb0208@gmail.com>
2024-11-14 22:03:39 +0900

    #198 Refactor: 필요없는 repository method 삭제

--- src/main/java/com/drinkeg/drinkeg/repository/WineRepository.java
+++ src/main/java/com/drinkeg/drinkeg/repository/WineRepository.java
@@ -1,8 +1,6 @@
 package com.drinkeg.drinkeg.repository;
 
-import com.drinkeg.drinkeg.domain.Member;
 import com.drinkeg.drinkeg.domain.Wine;
-import com.drinkeg.drinkeg.dto.HomeDTO.RecommendWineDTO;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,17 +12,5 @@ public interface WineRepository extends JpaRepository<Wine, Long>, WineRepositor
 
     // 검색한 와인 이름이 포함된 모든 와인을 찾는다.
     List<Wine> findAllByNameContainingIgnoreCaseOrderByName(String name);
-
-    // 추천 와인 조회를 위한 쿼리
-    List<Wine> findAllBySortContainingIgnoreCase(String sort);
-    List<Wine> findAllByAreaContainingIgnoreCase(String area);
-    List<Wine> findAllByPriceIsLessThanEqual(int price);
-
-    // Wine 조회 시 WineNote 도 같이 조회
-    @Query("SELECT w FROM Wine w JOIN FETCH w.wineNote WHERE w.id = :id")
-    Optional<Wine> findByIdWithWineNote(@Param("id") Long id);
-
-    @Query("SELECT w FROM Wine w JOIN FETCH w.tastingNoteList WHERE w.id = :id")
-    Optional<Wine> findByIdWithTastingNote(@Param("id") Long id);
 
 }
