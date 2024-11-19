9530d8ebd1db5771eeb1f5901e706e437b502736
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2024-11-19 09:48:13 +0900

    #177 refactor: 와인 강의 완료 리팩토링 - get메소드가 존재하지 않아서 dsl은 인터페이스까지 생성 - 수강완료 삭제 인자를 기존 wineLectureCompleteId에서 wineLectureId와 memberId를 기준으로 변경 - 유저의 완료한 강의 보기 기능 삭제

--- src/main/java/com/drinkeg/drinkeg/repository/wineLectureComplete/WineLectureCompleteRepository.java
+++ src/main/java/com/drinkeg/drinkeg/repository/wineLectureComplete/WineLectureCompleteRepository.java
@@ -1,4 +1,4 @@
-package com.drinkeg.drinkeg.repository;
+package com.drinkeg.drinkeg.repository.wineLectureComplete;
 
 import com.drinkeg.drinkeg.domain.Member;
 import com.drinkeg.drinkeg.domain.WineLecture;
@@ -6,9 +6,12 @@ import com.drinkeg.drinkeg.domain.WineLectureComplete;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface WineLectureCompleteRepository extends JpaRepository<WineLectureComplete, Long> {
+public interface WineLectureCompleteRepository extends JpaRepository<WineLectureComplete, Long>, WineLectureCompleteRepositoryCustom {
     List<WineLectureComplete> findAllByMember(Member member);
 
     boolean existsByWineLectureAndMember(WineLecture wineLecture, Member member);
+
+    Optional<WineLectureComplete> findByWineLectureIdAndMemberId(Long wineLectureId, Long memberId);
 }
