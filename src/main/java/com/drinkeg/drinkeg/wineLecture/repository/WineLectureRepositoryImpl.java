2d091d5f25b093d9b94d3f524418be0ffc4d0b64
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2024-12-26 14:41:57 +0900

    #196 fix: dsl 오류 해결

--- src/main/java/com/drinkeg/drinkeg/wineLecture/repository/WineLectureRepositoryImpl.java
+++ src/main/java/com/drinkeg/drinkeg/wineLecture/repository/WineLectureRepositoryImpl.java
@@ -11,7 +11,8 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.drinkeg.drinkeg.wineLecture.domain.QWineLecture.wineLecture;
-import static com.drinkeg.drinkeg.wineLecture.domain.QWineLectureComplete.wineLectureComplete;
+import static com.drinkeg.drinkeg.wineLectureComplete.domain.QWineLectureComplete.wineLectureComplete;
+
 
 @Repository
 @RequiredArgsConstructor
