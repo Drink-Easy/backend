7ee393e19dbd7baa7ab5c7ef83429d51379ebfe4
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2025-01-01 17:34:10 +0900

    #296 feat: 공지사항 api swagger 설명 추가

--- src/main/java/com/drinkeg/drinkeg/domain/notice/controller/NoticeController.java
+++ src/main/java/com/drinkeg/drinkeg/domain/notice/controller/NoticeController.java
@@ -4,6 +4,7 @@ import com.drinkeg.drinkeg.domain.notice.domain.Notice;
 import com.drinkeg.drinkeg.domain.notice.dto.NoticeResponse;
 import com.drinkeg.drinkeg.domain.notice.service.NoticeService;
 import com.drinkeg.drinkeg.global.apipayLoad.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +17,20 @@ import java.util.List;
 public class NoticeController {
     private final NoticeService noticeService;
 
+    @Operation(
+            summary = "공지사항 전체 조회",
+            description = "DB에 등록된 모든 공지사항을 조회한다. 단일조회와 결과값은 차이가 없다."
+    )
     @GetMapping("/notice")
     public ApiResponse<List<NoticeResponse>> getAllNotices() {
         List<NoticeResponse> notices = noticeService.findAllNotice();
         return ApiResponse.onSuccess(notices);
     }
 
+    @Operation(
+            summary = "공지사항 단일 조회",
+            description = "공지사항 Id로 공지사항을 조회한다."
+    )
     @GetMapping("/notice/{id}")
     public ApiResponse<NoticeResponse> getNoticeById(@PathVariable Long id) {
         NoticeResponse notice = noticeService.findNoticeById(id);
