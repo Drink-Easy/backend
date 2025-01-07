3740601a187b2e0e6aaea593e76ec1bf13ebcb55
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2025-01-07 20:03:26 +0900

    #318 refactor : controller swagger 설명 수정

--- src/main/java/com/drinkeg/drinkeg/domain/wine/controller/WineController.java
+++ src/main/java/com/drinkeg/drinkeg/domain/wine/controller/WineController.java
@@ -48,38 +48,31 @@ public class WineController {
 
     // todo: 페이징 구현하기, IOS 에 sort 타입 설명하기.
     @GetMapping("/review/{wineId}")
-    @Operation(summary = "와인 리뷰 전체 조회", description = "선택한 와인의 리뷰들을 List에 담아서 반환한다.")
+    @Operation(summary = "와인 리뷰 전체 조회", description = "선택한 와인의 리뷰들을 List에 담아서 반환한다." +
+            " 정렬 기준(sortType)은 \"최신순\", \"오래된 순\",\" 별점 높은 순\", \"별점 낮은 순\"으로 설정할 수 있다.")
     public ApiResponse<List<WineReviewResponse>> showWineReview(@PathVariable("wineId") Long wineId, @RequestParam String sortType) {
         List<WineReviewResponse> wineReviewResponseList = wineService.getWineReviewsAndIsLikedByWineId(wineId, SortType.of(sortType));
 
         return ApiResponse.onSuccess(wineReviewResponseList);
     }
 
-    // 홈화면 추천 와인 반환
     @GetMapping("/recommend")
-    @Operation(summary = "홈화면 페이지", description = "추천 와인 10개를 List로 반환")
+    @Operation(summary = "홈화면 페이지 추천 와인 조회", description = "추천 와인 10개를 List에 담아서 반환한다.")
     public ApiResponse<List<HomeWineResponse>> home(@AuthenticationPrincipal PrincipalDetail principalDetail,
                                                     HttpServletResponse response) {
+        List<HomeWineResponse> recommendWineList = wineService.getRecommendWineList(principalDetail.getUsername());
 
-        // 추천 와인 리스트 가져오기
-        List<HomeWineResponse> recommendWineList =
-                wineService.getRecommendWineList(principalDetail.getUsername());
-
-        // 응답 헤더에 Cache-Control 추가
         response.setHeader("Cache-Control", "max-age=3600, public");
 
         return ApiResponse.onSuccess(recommendWineList);
     }
 
-    // 홈화면 인기 와인 반환
     @GetMapping("/most-liked")
-    @Operation(summary = "홈화면 페이지", description = "인기 와인 10개를 List로 반환")
+    @Operation(summary = "홈화면 페이지 인기 와인 조회", description = "인기 와인 10개를 List에 담아서 반환한다.")
     public ApiResponse<List<HomeWineResponse>> mostLikedWine(HttpServletResponse response) {
 
-        // 인기 와인 리스트 가져오기
         List<HomeWineResponse> mostLikedWineList = wineService.getMostLikedWineList();
 
-        // 응답 헤더에 Cache-Control 추가
         response.setHeader("Cache-Control", "max-age=3600, public");
         return ApiResponse.onSuccess(mostLikedWineList);
     }
