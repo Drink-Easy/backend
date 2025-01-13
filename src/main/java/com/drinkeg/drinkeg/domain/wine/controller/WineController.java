940b1f5c2ea3104cdfdb3b502701e34364c32957
jeongyun1206 <67818407+jeongyun1206@users.noreply.github.com>
2025-01-13 17:03:11 +0900

    #322 docs: 페이징 관련 swagger 설명 추가

--- src/main/java/com/drinkeg/drinkeg/domain/wine/controller/WineController.java
+++ src/main/java/com/drinkeg/drinkeg/domain/wine/controller/WineController.java
@@ -32,7 +32,8 @@ public class WineController {
     private final WineService wineService;
 
     @GetMapping
-    @Operation(summary = "와인 검색", description = "와인 이름 또는 영어 이름으로 검색하여 와인의 기본 정보를 조회한다.")
+    @Operation(summary = "와인 검색", description = "와인 이름 또는 영어 이름으로 검색하여 와인의 기본 정보를 조회한다. " +
+            "paging 기능의 sort는 디폴트 값(name)을 사용하는 것을 권장한다.")
     public ApiResponse<List<WinePreviewResponse>> searchWine(
             @RequestParam(defaultValue = "") String searchName,
             @ParameterObject @PageableDefault(size = 10, sort = "name") Pageable pageable) {
@@ -56,8 +57,9 @@ public class WineController {
 
     // todo: 페이징 구현하기
     @GetMapping("/review/{wineId}")
-    @Operation(summary = "와인 리뷰 전체 조회", description = "선택한 와인의 리뷰들을 List에 담아서 반환한다." +
-            " 정렬 기준(sortType)은 \"최신순\", \"오래된 순\",\" 별점 높은 순\", \"별점 낮은 순\"으로 설정할 수 있다.")
+    @Operation(summary = "와인 리뷰 전체 조회", description = "선택한 와인의 리뷰들을 List에 담아서 반환한다. " +
+            " 정렬 기준(sortType)은 \"최신순\", \"오래된 순\",\" 별점 높은 순\", \"별점 낮은 순\"으로 설정할 수 있다. "
+    +"paging 기능의 sort는 사용하지 않고 sortType을 사용한다.")
     public ApiResponse<List<WineReviewResponse>> showWineReview(
             @PathVariable("wineId") Long wineId,
             @RequestParam String sortType,
