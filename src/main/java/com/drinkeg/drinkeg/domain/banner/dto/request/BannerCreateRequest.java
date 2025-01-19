7da3623985b3350f51b6fdb9ffbcdca29606c016
woogieon8on <swp0927@naver.com>
2025-01-19 23:25:46 +0900

    #362 Refactor: 배너 관련 DTO 리팩토링

--- src/main/java/com/drinkeg/drinkeg/domain/banner/dto/request/BannerCreateRequest.java
+++ src/main/java/com/drinkeg/drinkeg/domain/banner/dto/request/BannerCreateRequest.java
@@ -1,9 +1,14 @@
 package com.drinkeg.drinkeg.domain.banner.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record BannerCreateRequest(
-        @Schema(description = "배너 관련 게시글 URL", example = "www.drinkeg.com")
-        String postUrl
-) {
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BannerCreateRequest {
+    @Schema(description = "배너 관련 게시글 URL", example = "www.drinkeg.com")
+    String postUrl;
 }
--- src/main/java/com/drinkeg/drinkeg/domain/banner/dto/response/BannerResponse.java
+++ src/main/java/com/drinkeg/drinkeg/domain/banner/dto/response/BannerResponse.java
@@ -1,4 +1,29 @@
 package com.drinkeg.drinkeg.domain.banner.dto.response;
 
-public record BannerResponse() {
+import com.drinkeg.drinkeg.domain.banner.domain.Banner;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BannerResponse {
+    @Schema(description = "배너 id", example = "1")
+    private Long id;
+    @Schema(description = "배너 이미지 url", example = "https://drinkeg.amazonaws.com/123")
+    private String imageUrl;
+    @Schema(description = "배너 관련 게시글 url", example = "www.drinkeg.com")
+    private String postUrl;
+
+    public static BannerResponse of(Banner banner) {
+        return BannerResponse.builder()
+                .id(banner.getId())
+                .imageUrl(banner.getImageUrl())
+                .postUrl(banner.getPostUrl())
+                .build();
+    }
 }
