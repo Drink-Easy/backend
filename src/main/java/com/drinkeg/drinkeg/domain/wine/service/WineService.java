5e14257475ec7c33205f4614bdf231fcb153f35f
jeongyeon0208 <githubb0208@gmail.com>
2025-01-20 12:56:37 +0900

    #360 Refactor: 불필요 import 삭제

--- src/main/java/com/drinkeg/drinkeg/domain/wine/service/WineService.java
+++ src/main/java/com/drinkeg/drinkeg/domain/wine/service/WineService.java
@@ -2,11 +2,8 @@ package com.drinkeg.drinkeg.domain.wine.service;
 
 import com.drinkeg.drinkeg.domain.wine.dto.response.*;
 import com.drinkeg.drinkeg.domain.wine.repository.dto.SortType;
-import com.opencsv.exceptions.CsvException;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface WineService {
