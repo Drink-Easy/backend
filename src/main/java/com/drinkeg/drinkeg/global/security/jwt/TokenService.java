33023c2a8ffc894f71ff83cf719f79427692cea1
dayeong <dayeongyun184@gmail.com>
2025-02-05 12:06:02 +0900

    밀리초에서 초로 변경

--- src/main/java/com/drinkeg/drinkeg/global/security/jwt/TokenService.java
+++ src/main/java/com/drinkeg/drinkeg/global/security/jwt/TokenService.java
@@ -36,7 +36,7 @@ public class TokenService {
                 .secure(true) // HTTPS만 허용
                 .path("/")
                 .sameSite("Strict") // SameSite 설정
-                .maxAge(expierd) // 1일
+                .maxAge(expierd/1000)
                 .build();
         response.addHeader("Set-Cookie", cookie.toString());
 
