a3c0f3537a7464c577fb443cba2c8c60d2de4fe6
woogieon8on <swp0927@naver.com>
2025-02-07 14:14:36 +0900

    #402 Refactor: ReasonDTO 형식 통일

--- src/main/java/com/drinkeg/drinkeg/global/apipayLoad/code/ReasonDTO.java
+++ src/main/java/com/drinkeg/drinkeg/global/apipayLoad/code/ReasonDTO.java
@@ -1,18 +1,20 @@
 package com.drinkeg.drinkeg.global.apipayLoad.code;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@JsonPropertyOrder({"isSuccess", "httpStatus", "code", "message"})
 public class ReasonDTO {
+    private final Boolean isSuccess;
     private final HttpStatus httpStatus;
-    private final boolean isSuccess;
     private final String code;
     private final String message;
 
     @Builder
-    public ReasonDTO(HttpStatus httpStatus, boolean isSuccess, String code, String message) {
+    public ReasonDTO(HttpStatus httpStatus, Boolean isSuccess, String code, String message) {
         this.httpStatus = httpStatus;
         this.isSuccess = isSuccess;
         this.code = code;
