68b40f57ee84bc77cf0e6d4185b76d83b8b5910b
jeongyeon0208 <githubb0208@gmail.com>
2025-02-07 23:03:45 +0900

    #413 Refactor: 보유 와인 추가 시 가격 범위 validate 추가

--- src/main/java/com/drinkeg/drinkeg/domain/myWine/controller/request/MyWineRequest.java
+++ src/main/java/com/drinkeg/drinkeg/domain/myWine/controller/request/MyWineRequest.java
@@ -1,5 +1,7 @@
 package com.drinkeg.drinkeg.domain.myWine.controller.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -16,6 +18,8 @@ public class MyWineRequest {
     @NotNull(message = "구매 날짜 입력은 필수입니다.")
     private LocalDate purchaseDate;
 
+    @Min(value = 0, message = "구매 가격은 0 이상이어야 합니다.")
+    @Max(value = 1000000000, message = "구매 가격은 1000000000 이하여야 합니다.")
     private Integer purchasePrice;
 
     @Builder
