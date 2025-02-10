d7240409d3db0da61abee8b303cae9d66499b826
jeongyeon0208 <githubb0208@gmail.com>
2025-02-10 17:01:23 +0900

    #430 Refactor: likeIgnoreCase에서 like로 수정

--- src/main/java/com/drinkeg/drinkeg/domain/wine/repository/WineRepositoryImpl.java
+++ src/main/java/com/drinkeg/drinkeg/domain/wine/repository/WineRepositoryImpl.java
@@ -38,7 +38,7 @@ public class WineRepositoryImpl implements WineRepositoryCustom {
     public List<Wine> searchByName(String searchName, Pageable pageable) {
         return queryFactory.selectFrom(wine)
                 .where(
-                        wine.searchName.likeIgnoreCase(searchName)
+                        wine.searchName.like(searchName)
                 )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -55,7 +55,7 @@ public class WineRepositoryImpl implements WineRepositoryCustom {
         return queryFactory.select(wine.count())
                 .from(wine)
                 .where(
-                        wine.searchName.likeIgnoreCase(searchName)
+                        wine.searchName.like(searchName)
                 )
                 .fetchOne();
     }
