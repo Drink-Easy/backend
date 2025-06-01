5065011e5d81503c4313ac2d0fefc6bce8508196
jeongyeon0208 <githubb0208@gmail.com>
2025-06-01 22:36:59 +0900

    refactor; WineWishlistContrllerTest 빈티지 적용 (#460)

--- src/test/java/com/drinkeg/drinkeg/domain/wineWishlist/controller/WineWishlistControllerTest.java
+++ src/test/java/com/drinkeg/drinkeg/domain/wineWishlist/controller/WineWishlistControllerTest.java
@@ -7,6 +7,7 @@ import com.drinkeg.drinkeg.global.exception.GeneralException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,11 +29,11 @@ public class WineWishlistControllerTest extends WineWishlistControllerTestSuppor
     void saveWineWishlistByWineIdAndUser() throws Exception {
         // given
         Long wineId = 1L;
-        when(wineWishlistService.createWineWishlist(wineId, "user"))
+        when(wineWishlistService.createWineWishlist(wineId, 2017, "user"))
                 .thenReturn(1L);
 
         // when // then
-        mockMvc.perform(post("/wine-wishlist/{wineId}", wineId)
+        mockMvc.perform(post("/wine-wishlist/{wineId}?vintageYear={vintageYear}", wineId, 2017)
                         .with(csrf()))
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -46,16 +47,32 @@ public class WineWishlistControllerTest extends WineWishlistControllerTestSuppor
     @MockMember
     void saveWineWishlistByWrongWine() throws Exception {
         // given
-        when(wineWishlistService.createWineWishlist(-1L, "user"))
+        when(wineWishlistService.createWineWishlist(-1L, 2017,"user"))
                 .thenThrow(new GeneralException(ErrorStatus.WINE_NOT_FOUND));
 
         // when // then
-        mockMvc.perform(post("/wine-wishlist/{wineId}", -1L)
+        mockMvc.perform(post("/wine-wishlist/{wineId}?vintageYear={vintageYear}", -1L, 2017)
                         .with(csrf()))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code").value("WINE4001"))
                 .andExpect(jsonPath("$.message").value("와인이 없습니다."));
+    }
+
+    @DisplayName("빈티지 연도가 1970~2024 사이가 아닌 경우 위시리스트 추가 요청이 들어오면 예외가 발생한다.")
+    @Test
+    @MockMember
+    void saveWineWishlistWithInvalidVintageYear() throws Exception {
+        // given
+        int invalidVintageYear = 1969; // 1970보다 작은 값
+
+        // when // then
+        mockMvc.perform(post("/wine-wishlist/{wineId}?vintageYear={vintageYear}", 1L, invalidVintageYear)
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("CONSTRAINT_VIOLATION"))
+                .andExpect(jsonPath("$.message").value("빈티지는 1970~2024 사이여야 합니다."));
     }
 
     @DisplayName("이미 존재하는 위시리스트에 대해 추가 요청이 들어오면 예외가 발생한다.")
@@ -64,11 +81,11 @@ public class WineWishlistControllerTest extends WineWishlistControllerTestSuppor
     void saveDuplicatedWineWishlist() throws Exception {
         // given
         GeneralException generalException = new GeneralException(ErrorStatus.WINE_WISHLIST_ALREADY_EXISTS);
-        when(wineWishlistService.createWineWishlist(1L, "user"))
+        when(wineWishlistService.createWineWishlist(1L, 2017,"user"))
                 .thenThrow(generalException);
 
         // when // then
-        mockMvc.perform(post("/wine-wishlist/{wineId}", 1L)
+        mockMvc.perform(post("/wine-wishlist/{wineId}?vintageYear={vintageYear}", 1, 2017)
                         .with(csrf()))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
@@ -82,8 +99,8 @@ public class WineWishlistControllerTest extends WineWishlistControllerTestSuppor
     void getWineWishlistByUser() throws Exception {
         // given
         List<WinePreviewResponse> winePreviewResponses = List.of(
-                createWinePreviewResponse(1L, "와인1", "wine1", "https://image1", "레드", "프랑스", "보르도", "메를로", 4.5f, 10000),
-                createWinePreviewResponse(2L, "와인2", "wine2", "https://image2", "화이트", "이탈리아", "피에몬테", "네비올로", 4.0f, 20000)
+                createWinePreviewResponse(1L, 2015,"와인1", "https://image1", "레드", "프랑스", "보르도", "메를로", 4.5f, 10000),
+                createWinePreviewResponse(2L, 2016,"와인2", "https://image2", "화이트", "이탈리아", "피에몬테", "네비올로", 4.0f, 20000)
         );
         when(wineWishlistService.getAllWineWishlistByMember("user"))
                 .thenReturn(winePreviewResponses);
@@ -99,7 +116,7 @@ public class WineWishlistControllerTest extends WineWishlistControllerTestSuppor
 
                 .andExpect(jsonPath("$.result[0].wineId").value(1))
                 .andExpect(jsonPath("$.result[0].name").value("와인1"))
-                .andExpect(jsonPath("$.result[0].nameEng").value("wine1"))
+                .andExpect(jsonPath("$.result[0].vintageYear").value(2015))
                 .andExpect(jsonPath("$.result[0].imageUrl").value("https://image1"))
                 .andExpect(jsonPath("$.result[0].sort").value("레드"))
                 .andExpect(jsonPath("$.result[0].country").value("프랑스"))
@@ -110,7 +127,7 @@ public class WineWishlistControllerTest extends WineWishlistControllerTestSuppor
 
                 .andExpect(jsonPath("$.result[1].wineId").value(2))
                 .andExpect(jsonPath("$.result[1].name").value("와인2"))
-                .andExpect(jsonPath("$.result[1].nameEng").value("wine2"))
+                .andExpect(jsonPath("$.result[1].vintageYear").value(2016))
                 .andExpect(jsonPath("$.result[1].imageUrl").value("https://image2"))
                 .andExpect(jsonPath("$.result[1].sort").value("화이트"))
                 .andExpect(jsonPath("$.result[1].country").value("이탈리아"))
@@ -160,10 +177,10 @@ public class WineWishlistControllerTest extends WineWishlistControllerTestSuppor
     void deleteWineWishlistByWrongWine() throws Exception {
         // given
         doThrow(new GeneralException(ErrorStatus.WINE_NOT_FOUND))
-                .when(wineWishlistService).deleteWineWishlist(-1L, "user");
+                .when(wineWishlistService).deleteWineWishlist(-1L, 2017, "user");
 
         // when // then
-        mockMvc.perform(delete("/wine-wishlist/{wineId}", -1L)
+        mockMvc.perform(delete("/wine-wishlist/{wineId}?vintageYear={vintageYear}", -1L, 2017)
                         .with(csrf()))
                 .andDo(print())
                 .andExpect(status().isBadRequest())
@@ -171,11 +188,11 @@ public class WineWishlistControllerTest extends WineWishlistControllerTestSuppor
                 .andExpect(jsonPath("$.message").value("와인이 없습니다."));
     }
 
-    private WinePreviewResponse createWinePreviewResponse(Long wineId, String name, String nameEng, String imageUrl, String sort, String country, String region, String variety, float vivinoRating, int price) {
+    private WinePreviewResponse createWinePreviewResponse(Long wineId, Integer vintageYear, String name, String imageUrl, String sort, String country, String region, String variety, float vivinoRating, int price) {
         return WinePreviewResponse.builder()
                 .wineId(wineId)
+                .vintageYear(vintageYear)
                 .name(name)
-                .nameEng(nameEng)
                 .imageUrl(imageUrl)
                 .sort(sort)
                 .country(country)
