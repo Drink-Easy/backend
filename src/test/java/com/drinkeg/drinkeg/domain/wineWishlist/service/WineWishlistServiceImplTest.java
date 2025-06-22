a293f4393e9ce970a2aec036b395bde3f8fcfbad
jeongyeon0208 <githubb0208@gmail.com>
2025-06-22 18:39:29 +0900

    test: WineWishlistServiceImplTest 빈티지 적용 (#460)

--- src/test/java/com/drinkeg/drinkeg/domain/wineWishlist/service/WineWishlistServiceImplTest.java
+++ src/test/java/com/drinkeg/drinkeg/domain/wineWishlist/service/WineWishlistServiceImplTest.java
@@ -8,6 +8,8 @@ import com.drinkeg.drinkeg.domain.wine.domain.Wine;
 import com.drinkeg.drinkeg.domain.wine.domain.WineNoteStatistics;
 import com.drinkeg.drinkeg.domain.wine.dto.response.WinePreviewResponse;
 import com.drinkeg.drinkeg.domain.wine.repository.WineRepository;
+import com.drinkeg.drinkeg.domain.wine.wineVintage.domain.WineVintage;
+import com.drinkeg.drinkeg.domain.wine.wineVintage.repository.WineVintageRepository;
 import com.drinkeg.drinkeg.domain.wineWishlist.domain.WineWishlist;
 import com.drinkeg.drinkeg.domain.wineWishlist.repository.WineWishlistRepository;
 import com.drinkeg.drinkeg.global.apipayLoad.code.status.ErrorStatus;
@@ -32,26 +34,51 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
     WineRepository wineRepository;
     @Autowired
     WineWishlistRepository wineWishlistRepository;
+    @Autowired
+    WineVintageRepository wineVintageRepository;
 
     @Autowired
     WineWishlistService wineWishlistService;
 
-    @DisplayName("와인 위시리스트를 저장한다.")
+    @DisplayName("빈티지를 선택하고 와인 위시리스트를 저장한다.")
     @Test
     void createWineWishlist() {
         //given
         Member member = memberRepository.save(createMember("user1"));
         Wine wine = wineRepository.save(createWine("wine1"));
+        saveWineVintage(wine, 2017);
 
         //when
-        Long wineWishlistId = wineWishlistService.createWineWishlist(wine.getId(), member.getUsername());
+        Long wineWishlistId = wineWishlistService.createWineWishlist(wine.getId(), 2017, member.getUsername());
 
         //then
         Optional<WineWishlist> wineWishlist = wineWishlistRepository.findById(wineWishlistId);
         Assertions.assertThat(wineWishlist).isPresent();
         wineWishlist.ifPresent(note -> {
             assertThat(note.getMember().getId()).isEqualTo(member.getId());
-            assertThat(note.getWine().getId()).isEqualTo(wine.getId());
+            assertThat(note.getWineVintage().getWine().getId()).isEqualTo(wine.getId());
+            assertThat(note.getWineVintage().getVintageYear()).isEqualTo(2017);
+        });
+    }
+
+    @DisplayName("빈티지를 선택하지 않고 와인 위시리스트를 저장하면 빈티지 연도가 0으로 저장된다.")
+    @Test
+    void createWineWishlistWithoutVintage() {
+        //given
+        Member member = memberRepository.save(createMember("user1"));
+        Wine wine = wineRepository.save(createWine("wine1"));
+        saveWineVintage(wine, 0);
+
+        //when
+        Long wineWishlistId = wineWishlistService.createWineWishlist(wine.getId(), null, member.getUsername());
+
+        //then
+        Optional<WineWishlist> wineWishlist = wineWishlistRepository.findById(wineWishlistId);
+        Assertions.assertThat(wineWishlist).isPresent();
+        wineWishlist.ifPresent(note -> {
+            assertThat(note.getMember().getId()).isEqualTo(member.getId());
+            assertThat(note.getWineVintage().getWine().getId()).isEqualTo(wine.getId());
+            assertThat(note.getWineVintage().getVintageYear()).isEqualTo(0);
         });
     }
 
@@ -62,9 +89,10 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
         Member member = memberRepository.save(createMember("user1"));
         memberRepository.delete(member);
         Wine wine = wineRepository.save(createWine("wine1"));
+        saveWineVintage(wine, 2017);
 
         //when && then
-        assertThatThrownBy(() -> wineWishlistService.createWineWishlist(wine.getId(), "user1"))
+        assertThatThrownBy(() -> wineWishlistService.createWineWishlist(wine.getId(), 2017,"user1"))
                 .isInstanceOf(GeneralException.class)
                 .hasMessage(ErrorStatus.MEMBER_NOT_FOUND.getMessage());
     }
@@ -76,7 +104,7 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
         Member member = memberRepository.save(createMember("user1"));
 
         //when && then
-        assertThatThrownBy(() -> wineWishlistService.createWineWishlist(-1L, member.getUsername()))
+        assertThatThrownBy(() -> wineWishlistService.createWineWishlist(-1L, 2017, member.getUsername()))
                 .isInstanceOf(GeneralException.class)
                 .hasMessage(ErrorStatus.WINE_NOT_FOUND.getMessage());
     }
@@ -87,10 +115,14 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
         //given
         Member member = memberRepository.save(createMember("user1"));
         Wine wine = wineRepository.save(createWine("wine1"));
-        wineWishlistRepository.save(WineWishlist.create(member, wine));
+        WineVintage wineVintage = saveWineVintage(wine, 2017);
+        wineWishlistRepository.save(WineWishlist.builder()
+                .member(member)
+                .wineVintage(wineVintage)
+                .build());
 
         //when && then
-        assertThatThrownBy(() -> wineWishlistService.createWineWishlist(wine.getId(), member.getUsername()))
+        assertThatThrownBy(() -> wineWishlistService.createWineWishlist(wine.getId(), 2017, member.getUsername()))
                 .isInstanceOf(GeneralException.class)
                 .hasMessage(ErrorStatus.WINE_WISHLIST_ALREADY_EXISTS.getMessage());
     }
@@ -103,21 +135,25 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
         Wine wine1 = wineRepository.save(createWineDetail("와인1","wine1", "http://default.image1", "레드", "프랑스", "보르도", "샤도네이", 4.1f, 100));
         Wine wine2 = wineRepository.save(createWineDetail("와인2","wine2", "http://default.image2", "화이트", "이탈리아", "로마", "모스카토", 4.5f, 30));
         Wine wine3 = wineRepository.save(createWineDetail("와인3","wine3", "http://default.image3", "로제", "스페인", "바르셀로나", "페타이", 4.3f, 50));
-        wineWishlistRepository.save(WineWishlist.create(member, wine1));
-        wineWishlistRepository.save(WineWishlist.create(member, wine2));
-        wineWishlistRepository.save(WineWishlist.create(member, wine3));
+        WineVintage wineVintage1 = saveWineVintage(wine1, 2017);
+        WineVintage wineVintage2 = saveWineVintage(wine2, 2018);
+        WineVintage wineVintage3 = saveWineVintage(wine3, 2019);
+
+        wineWishlistRepository.save(WineWishlist.create(member, wineVintage1));
+        wineWishlistRepository.save(WineWishlist.create(member, wineVintage2));
+        wineWishlistRepository.save(WineWishlist.create(member, wineVintage3));
 
         //when
         List<WinePreviewResponse> allWineWishlistByMember = wineWishlistService.getAllWineWishlistByMember(member.getUsername());
 
         //then
         assertThat(allWineWishlistByMember).hasSize(3)
-                .extracting("wineId", "name", "nameEng", "imageUrl",
+                .extracting("wineId", "name", "vintageYear","imageUrl",
                         "sort", "country", "region", "variety", "vivinoRating", "price")
                 .containsExactly(
-                        tuple(wine3.getId(), "와인3" ,"wine3", "http://default.image3", "로제", "스페인", "바르셀로나", "페타이", 4.3f, 50),
-                        tuple(wine2.getId(), "와인2" ,"wine2", "http://default.image2", "화이트", "이탈리아", "로마", "모스카토", 4.5f, 30),
-                        tuple(wine1.getId(), "와인1" ,"wine1", "http://default.image1", "레드", "프랑스", "보르도", "샤도네이", 4.1f, 100)
+                        tuple(wine3.getId(), "와인3", 2019, "http://default.image3", "로제", "스페인", "바르셀로나", "페타이", 4.3f, 50),
+                        tuple(wine2.getId(), "와인2", 2018, "http://default.image2", "화이트", "이탈리아", "로마", "모스카토", 4.5f, 30),
+                        tuple(wine1.getId(), "와인1" ,2017, "http://default.image1", "레드", "프랑스", "보르도", "샤도네이", 4.1f, 100)
 
                 );
     }
@@ -144,9 +180,9 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
 
         Member member = memberRepository.save(createMember("user1"));
         Wine wine1 = wineRepository.save(createWineDetail("와인1","wine1", "http://default.image1", "레드", "프랑스", "보르도", "샤도네이", 4.1f, 100));
-        wineWishlistRepository.save(WineWishlist.create(member, wine1));
+        saveWineVintage(wine1, 2017);
 
-        //when & then
+                //when & then
         assertThatThrownBy(() -> wineWishlistService.getAllWineWishlistByMember("wrongUser"))
                 .isInstanceOf(GeneralException.class)
                 .hasMessage(ErrorStatus.MEMBER_NOT_FOUND.getMessage());
@@ -158,11 +194,12 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
         //given
         Member member = memberRepository.save(createMember("user1"));
         Wine wine = wineRepository.save(createWine("wine1"));
-        WineWishlist wishlist = WineWishlist.create(member, wine);
+        WineVintage wineVintage1 = saveWineVintage(wine, 2017);
+        WineWishlist wishlist = WineWishlist.create(member, wineVintage1);
         wineWishlistRepository.save(wishlist);
 
         //when
-        wineWishlistService.deleteWineWishlist(wine.getId(), member.getUsername());
+        wineWishlistService.deleteWineWishlist(wine.getId(), 2017, member.getUsername());
 
         //then
         assertThat(wineWishlistRepository.existsById(wishlist.getId())).isFalse();
@@ -177,10 +214,11 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
 
         Member member = memberRepository.save(createMember("user1"));
         Wine wine = wineRepository.save(createWine("wine1"));
-        wineWishlistRepository.save(WineWishlist.create(member, wine));
+        WineVintage wineVintage1 = saveWineVintage(wine, 2017);
+        wineWishlistRepository.save(WineWishlist.create(member, wineVintage1));
 
         //when & then
-        assertThatThrownBy(() -> wineWishlistService.deleteWineWishlist(wine.getId(), "wrongUser"))
+        assertThatThrownBy(() -> wineWishlistService.deleteWineWishlist(wine.getId(), 2017,"wrongUser"))
                 .isInstanceOf(GeneralException.class)
                 .hasMessage(ErrorStatus.MEMBER_NOT_FOUND.getMessage());
     }
@@ -192,7 +230,7 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
         Member member = memberRepository.save(createMember("user1"));
 
         //when & then
-        assertThatThrownBy(() -> wineWishlistService.deleteWineWishlist(-1L, member.getUsername()))
+        assertThatThrownBy(() -> wineWishlistService.deleteWineWishlist(-1L, 2017, member.getUsername()))
                 .isInstanceOf(GeneralException.class)
                 .hasMessage(ErrorStatus.WINE_NOT_FOUND.getMessage());
     }
@@ -203,9 +241,10 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
         //given
         Member member = memberRepository.save(createMember("user1"));
         Wine wine = wineRepository.save(createWine("wine1"));
+        saveWineVintage(wine, 2017);
 
         //when & then
-        assertThatThrownBy(() -> wineWishlistService.deleteWineWishlist(wine.getId(), member.getUsername()))
+        assertThatThrownBy(() -> wineWishlistService.deleteWineWishlist(wine.getId(),2017, member.getUsername()))
                 .isInstanceOf(GeneralException.class)
                 .hasMessage(ErrorStatus.WINE_WISHLIST_NOT_FOUND.getMessage());
     }
@@ -243,5 +282,13 @@ class WineWishlistServiceImplTest extends IntegrationTestSupport {
                 .role(Role.ROLE_USER)
                 .isFirst(false)
                 .build();
+    }
+
+    private WineVintage saveWineVintage(Wine wine, int vintageYear) {
+        return wineVintageRepository.save(WineVintage.builder()
+                .wine(wine)
+                .vintageYear(vintageYear)
+                .wineNoteStatistics(WineNoteStatistics.create())
+                .build());
     }
 }
